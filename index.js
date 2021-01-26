9834f56d7f4652c8732886afe374c9dc97046280
Gerardo Salas Montoya <gesalas@cr.ibm.com>
2021-01-26 15:39:50 -0600

    removing api route

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })) ;
 app.use(cors());
-app.use('/api',router);
+app.use('/',router);
 //app.get('/', (req, res) => { res.status(200).send('Server is working.') }); 
 app.listen(port, () => { console.log(`🌏 Server is running at http://localhost:${port}`) });
 
